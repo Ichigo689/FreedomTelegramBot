81e97f7765a47928bb171cd8397ae73ca754be4b
SabbirBaser <sbaser2015@my.fit.edu>
2016-07-04 16:23:21 -0400

    Port

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ var botApi = require('node-telegram-bot-api');
 // var imgSearch = googleImages(cseId, imgAPI);
 var bot = new botApi(token, {polling: true});
 var newImgSearch = require('g-i-s');
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoiding Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
+
 
 var searches = [
     'American Flag',
