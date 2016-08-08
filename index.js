eb71c3d0eafa35073b8fe0250d3f1673c66f6dde
Boats McGee <Boats McGee>
2016-08-07 22:30:40 -0400

    Oops, forgot to add app stuff!

--- index.js
+++ index.js
@@ -14,15 +14,15 @@ var express = require('express');
 var app = express();
 var util = require('util');
 
-// app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
-// //For avoiding Heroku $PORT error
-// app.get('/', function(request, response) {
-//     var result = 'App is running'
-//     response.send(result);
-// }).listen(app.get('port'), function() {
-//     console.log('App is running, server is listening on port ', app.get('port'));
-// });
+//For avoiding Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 
 
